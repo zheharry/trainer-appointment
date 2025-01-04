67d939d98102586f669ed89dd4daa5934e94d9cc
Cheng, Harry <zheharry@gmail.com>
2025-01-04 20:16:07 +0800

    TrainerBookingApp

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //import './index.css'
-//import TrainerBookingApp from './TrainerBookingApp.jsx'
-import App from './App.jsx'
+import TrainerBookingApp from './TrainerBookingApp.jsx'
+//import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <TrainerBookingApp />
   </StrictMode>,
 )
